fff7a4e5283d72fc9fafe238b6539d58f1831f49
Marcin Smola <marcin_smola@epam.com>
2018-10-04 15:48:21 +0200

    init commit

---
+++ receiver/src/main/java/com/smola/Recv.java
@@ -0,0 +1,23 @@
+package com.smola;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recv {
+    private static final String QUEUE_NAME = "hello";
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+
+    }
+}
